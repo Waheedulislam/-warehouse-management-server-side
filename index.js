3ff3caafa94bf8772d44ba4efb64e7e33077765c
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-07 16:44:38 +0600

    post-method-done

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ async function run() {
             res.send(item);
         })
 
+        // POST
+        app.post('/item', async (req, res) => {
+            const newItem = req.body;
+            const result = await itemCollection.insertOne(newItem);
+            res.send(result);
+        })
+
+
     } finally {
         //   await client.close();
     }
