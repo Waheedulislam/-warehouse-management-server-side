7bfb0a3335412e58957b65d164bafd0aedd0a433
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-07 17:08:19 +0600

    delete-mathod-done

--- index.js
+++ index.js
@@ -39,6 +39,14 @@ async function run() {
             res.send(result);
         })
 
+        // Delete
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
     } finally {
         //   await client.close();
