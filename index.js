24c90ed0b734b0b467691b7f2d342fd826942aeb
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-08 10:49:13 +0600

    done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const res = require('express/lib/response');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -25,7 +26,7 @@ async function run() {
             const item = await cursor.toArray();
             res.send(item)
         });
-        app.get('/item/:id', async (req, res) => {
+        app.get('/itemDetails/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const item = await itemCollection.findOne(query);
@@ -37,6 +38,39 @@ async function run() {
             const newItem = req.body;
             const result = await itemCollection.insertOne(newItem);
             res.send(result);
+        })
+
+        // post 
+        app.put('/addQuantity', async (req, res) => {
+            const id = req.params.id;
+            const oldQuantity = parseInt(req.query.oldQuantity);
+            const newQuantity = parseInt(req.body.quantity);
+
+            const totalQuantity = oldQuantity + newQuantity;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: totalQuantity.quantity
+                }
+            };
+            const result = await itemCollection.updateOne(filter, updatedDoc, options)
+            res.send(result)
+        })
+
+        // put update  user
+        app.put('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedUser.newQuantity
+                }
+            };
+            const result = await itemCollection.updateOne(filter, updatedDoc, options)
+            res.send(result)
         })
 
         // Delete
